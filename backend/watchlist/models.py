fc6ce11c97616f02a911bc9c82de8cb32cb25e3c
The-0-Gill-illyo <theogill1369@gmail.com>
2022-11-01 17:48:38 -0400

    Created watchlist app + Created table in views file + ran migrations

--- backend/watchlist/models.py
+++ backend/watchlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    watchlist = models.CharField(max_length=10)
+    target_price = models.DecimalField(max_digits=19, decimal_places=2)
